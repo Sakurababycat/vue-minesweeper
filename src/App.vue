aeea137ef8fd2539652483d5574f0fbedf4a3841
Sakurababycat <63246567+Sakurababycat@users.noreply.github.com>
2023-09-19 21:09:54 +0800

    add assistant

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,9 @@ export default {
       time: 0,
       timer: 0,
       minecnt: 0,
+      help: false,
+      helpUsed: false,
+      lastSweepTime: 0,
     };
   },
   watch: {
@@ -85,6 +88,10 @@ export default {
       this.minecnt = this.mines;
       this.time = 0;
       clearInterval(this.timer);
+      this.help = false;
+      this.helpUsed = false;
+      this.lastSweepTime = 0;
+
     },
     setMines(crow: number, ccol: number) {
 
@@ -110,6 +117,9 @@ export default {
           }
           else {
             this.time++;
+            if (this.help == false && this.helpUsed == false && this.time - this.lastSweepTime >= 20) { // this.lastSweepTime != 0 && 
+              this.help = true;
+            }
           }
         }, 1000);
       }
@@ -126,6 +136,13 @@ export default {
       cell.opened = true;
 
       if (cell.mine) {
+        if (this.help && this.helpUsed) {
+          cell.opened = false;
+          this.flagCell(row, column);
+          this.help = false;
+          return;
+        }
+        this.help = false;
         cell.exploded = true;
         this.gameOver = true;
         this.win = false
@@ -144,6 +161,8 @@ export default {
         return;
       }
       this.sweepedCnt++;
+      this.lastSweepTime = this.time;
+      this.help = false;
 
       if (cell.content === '') {
         // 计算相邻格子的雷数
@@ -175,8 +194,9 @@ export default {
         const neighborFlags = neighbors.filter((neighbor) => neighbor.content === '☥');
         const neighborCanOpen = neighbors.filter((neighbor) => !neighbor.opened);
 
-        if (neighborFlags.length === neighborMines.length) {
+        if (neighborFlags.length === neighborMines.length || neighborFlags.length === neighborMines.length - Number(this.help && this.helpUsed)) {
           // 递归打开相邻的空格子
+          neighborCanOpen.filter((neighbor) => neighbor.content === '' && neighbor.mine).forEach((neighbor) => this.flagCell(neighbor.row, neighbor.col));
           neighbors.forEach((neighbor) => {
             this.openCell(neighbor.row, neighbor.col, false);
           });
@@ -270,11 +290,16 @@ export default {
       </div>
     </div>
     <h2 v-if="gameOver" class="minesweeper">{{ !win ? "游戏结束！你踩中了地雷。" : "游戏胜利" }}</h2>
-    <button class="reset" @click="resetBoard">重新开始</button>
+    <h4 v-if="help" class="minesweeper">{{ !helpUsed ? "点击下方按钮获取帮助，一局只能使用一次。" : "进入辅助状态，下一次地雷引爆将被抵消。" }}</h4>
+    <button v-if="!(help && !helpUsed)" class="reset" @click="resetBoard">重新开始</button>
+    <button v-else class="help" @click="() => { helpUsed = true; }">获取帮助</button>
   </div>
   <h4 style="color: red;">Tips:</h4>
+  <h4 v-if="help || helpUsed" style="color: darkgoldenrod; font-weight: bold; font-style: italic;">
+    帮助将在以下情况生效：打开任意格子；点击周围标记=雷数-1的格子（n选1雷）。效果：标记地雷并消耗帮助机会（只有一次）。</h4>
   <h5 style="color: darkcyan">左键开启方格，右键（取消）标记方格。</h5>
   <h5 style="color: lightseagreen">第一次点击一定不是雷，并且周围8个格子一定不是雷。</h5>
+  <h5 style="color: coral">每局有且仅有一次帮助机会，在20s内没有开启新格子时触发。</h5>
   <h5 style="color: blue">点击任意数字标记：</h5>
   <h5 style="color: purple">如果周围未开格子与数字相同则标记所有格子。</h5>
   <h5 style="color: blueviolet">如果周围标记数量与数字相同则开启所有未标记格子。</h5>
